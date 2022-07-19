99ecc73f511edef3c3602c5ccc0024ec163d577a
HerikHenrique <74871202+HerikHenrique@users.noreply.github.com>
2022-07-19 18:13:32 -0300

    Add files via upload

---
+++ test/src/main/java/Resposta_4/Ingresso.java
@@ -0,0 +1,16 @@
+package Resposta_4;
+
+public class Ingresso {
+    double reais;
+
+    public double getReais() {
+        return reais;
+    }
+
+    public void setReais(double reais) {
+        this.reais = reais;
+    }
+    public double imprimeValor(){
+        return reais;
+    }
+}
---
+++ test/src/main/java/Resposta_4/Vip.java
@@ -0,0 +1,8 @@
+package Resposta_4;
+
+public class Vip extends Ingresso{
+
+    double adicional;
+
+
+}
